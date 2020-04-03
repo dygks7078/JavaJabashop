85d3d2614b0a7eefeeec0eb3e320bce62a25b770
sajh78 <redksa0708@gmail.com>
2020-04-03 18:01:58 +0900

    회원 수정 수정

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -16,7 +16,26 @@ public class MemberController {
 	public void setDao(MemberDao dao) {
 		this.dao = dao;
 	}
+	
+	@RequestMapping("")
+	public String getMember(String id) {
+		String str = "";
+		dao.getMember(id);
+		return str;
+	}
 
+	@RequestMapping("/all/updateMember")
+	public String updateMember(MemberVo m) {
+		String str ="수정 완료";
+		String m1 = m.getPwd();
+		System.out.println(m1);
+		MemberVo mv = new MemberVo();
+		String v = mv.getPwd();
+		System.out.println(v);
+		dao.updateMember(m);
+		System.out.println(str);
+		return str;
+	}
 	
 	@RequestMapping("/all/insertMember")
 	public String insertMember(MemberVo m) {
