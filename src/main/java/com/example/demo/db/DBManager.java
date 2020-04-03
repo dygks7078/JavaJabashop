70d3060a556e9d8b9d3b9b3b6f8c0847d712a179
dygks7078 <dygks7078@naver.com>
2020-04-03 13:04:56 +0900

    first commit

---
+++ src/main/java/com/example/demo/db/DBManager.java
@@ -0,0 +1,53 @@
+  
+package com.example.demo.db;
+
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+
+public class DBManager {
+	private static SqlSessionFactory factory;
+	static {
+		try {
+			Reader reader = Resources.getResourceAsReader("com/example/demo/db/sqlMapConfig.xml");
+			factory = new SqlSessionFactoryBuilder().build(reader);
+			reader.close();
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	
+	
+	
+//	
+//	public static List<MemberVo> selectMemberAll(){
+//		SqlSession session = factory.openSession();
+//		List<MemberVo> list  = session.selectList("member.selectAll");
+//		session.close();
+//		return list;
+//	}
+//	
+//	public static MemberVo selectMember(String username){
+//		SqlSession session = factory.openSession();
+//		MemberVo m = session.selectOne("member.selectMember",username);
+//		session.close();
+//		return m;
+//	}
+//
+//	public static int insertMember(MemberVo m) {
+//		int re =  -1;
+//		SqlSession session = factory.openSession();
+//		re= session.insert("member.insert",m);
+//		session.commit();
+//		session.close();
+//		return re;
+//	}
+}
