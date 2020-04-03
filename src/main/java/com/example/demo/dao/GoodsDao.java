a7572ee702db974d3e8518d4b463b520a7058cf0
jackdopark <jackdopark8@gmail.com>
2020-04-03 15:41:59 +0900

    커밋

--- src/main/java/com/example/demo/dao/GoodsDao.java
+++ src/main/java/com/example/demo/dao/GoodsDao.java
@@ -3,6 +3,7 @@ package com.example.demo.dao;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.db.DBManager;
+import com.example.demo.vo.GoodsVo;
 
 @Repository
 public class GoodsDao {
--- src/main/java/com/example/demo/db/DBManager.java
+++ src/main/java/com/example/demo/db/DBManager.java
@@ -9,6 +9,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import com.example.demo.vo.GoodsVo;
 import com.example.demo.vo.MemberVo;
 
 public class DBManager {
@@ -24,14 +25,12 @@ public class DBManager {
 		}
 	}
 	
-<<<<<<< HEAD
 	public static MemberVo selectMember(String username){
 		SqlSession session = factory.openSession();
 		MemberVo m = session.selectOne("member.selectMember", username);
 		session.close();
 		return m;
 	}
-=======
 	public static int deleteGoods(GoodsVo g) {
 		int re = -1;
 		SqlSession session = factory.openSession(true);
@@ -48,7 +47,6 @@ public class DBManager {
 		return re;
 	}
 	
->>>>>>> branch 'master' of https://github.com/dygks7078/JavaJabashop.git
 	
 	
 	public static int insertMember(MemberVo m) {
