4fa3663627241c2238214aa54655cc5ae3d8f35a
shuttlekim <kunkun7762@gmail.com>
2020-04-03 16:41:52 +0900

    order 제작중 [영현]

---
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.db.DBManager;
+import com.example.demo.vo.OrderVo;
+import com.google.gson.Gson;
+
+@RestController
+public class OrderController {
+
+	@GetMapping("/purchase")
+	public String purchase(OrderVo o) {
+		String str = "";
+		
+		
+		
+		return str;
+	}
+	
+	@GetMapping("/orderList")
+	public String orderList() {
+		
+		
+		return (new Gson()).toJson( DBManager.orderList() );
+	}
+}
