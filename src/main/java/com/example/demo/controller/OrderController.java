8f3f3e230f744010a747a0d25854096113bd83fe
shuttlekim <kunkun7762@gmail.com>
2020-04-03 18:04:46 +0900

    수정 [영현]

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -10,16 +10,31 @@ import com.google.gson.Gson;
 @RestController
 public class OrderController {
 
-	@GetMapping("/purchase")
+	@GetMapping("/all/order")
 	public String purchase(OrderVo o) {
 		String str = "";
 		
+		//주문한 상품의 재고 수량을 불러온다.
+		int goods_qty = DBManager.getGoods(o.getNo()).getQty();
 		
+		//재고수량이 주문수량보다 많은지 확인, 아니면 주문불가
+		if(o.getOrder_qty() <= goods_qty) {
+			
+			int re = DBManager.order(o);
+			if(re > 1) {
+				str = "성공적으로 주문하였습니다.";
+			}else {
+				str = "주문에 실패하였습니다.";
+			}
+			
+		}else {
+			str = "상품의 재고가 부족합니다.";
+		}
 		
 		return str;
 	}
 	
-	@GetMapping("/orderList")
+	@GetMapping("/all/orderList")
 	public String orderList() {
 		
 		return (new Gson()).toJson( DBManager.orderList() );
