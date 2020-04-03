4d785bebb111704d97c624ca489b7adde588664c
hyunsung0308 <hyunsung0308@gmail.com>
2020-04-03 18:13:47 +0900

    유현성 - 수정

--- src/main/java/com/example/demo/JavaJabashopApplication.java
+++ src/main/java/com/example/demo/JavaJabashopApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
@@ -10,6 +11,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 
 @SpringBootApplication
+@Configuration
 public class JavaJabashopApplication {
 
 	@Bean
